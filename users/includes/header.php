66a66371d5d8daa7ae893d6a0ef9afdf0c1922b0
Thaw <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2022-04-25 11:50:47 +0630

    fix small bug
    navbar url

--- users/includes/header.php
+++ users/includes/header.php
@@ -1,9 +1,9 @@
- <header class="header black-bg">
-              <div class="sidebar-toggle-box">
-                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
-              </div>
-            <!--logo start-->
-            <a href="index.html" class="logo"><b>KBTC Complaint Management System</b></a>
-                
-            
-        </header>
+<header class="header black-bg">
+    <div class="sidebar-toggle-box">
+        <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+    </div>
+    <!--logo start-->
+    <a href="index.php" class="logo"><b>KBTC Complaint Management System</b></a>
+
+
+</header>
